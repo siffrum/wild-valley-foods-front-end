306e0078f89c7361db8809ca6a163b32c1313ab8
siffrum <siffrumanalytics@gmail.com>
2025-08-05 19:18:34 +0530

    Added Top Nav Bar and Internal Components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './main/components/main/end-user/home/home';
 
 export const routes: Routes = [
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'home',component:Home},
     {
     path: 'auth',
     loadChildren: () => import('../app/main/auth/auth.module').then(m => m.AuthModule)
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopNavComponent } from './main/components/internal/top-nav/top-nav.component';
+import { Footer } from './main/components/internal/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,TopNavComponent,Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
