c72bd3e474b4e098ebaaec972e1e87f6a43f3a0a
Musaib <musaib709@outlook.com>
2025-08-25 22:08:53 +0530

    fixed loader

--- src/app/main/components/main/admin/dashboard/dashboard.component.ts
+++ src/app/main/components/main/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { BaseComponent } from '../../../../../base.component';
+import { CommonService } from '../../../../../services/common.service';
+import { LogHandlerService } from '../../../../../services/log-handler.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,6 +9,18 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent extends BaseComponent<any> {
+  constructor(commonService:CommonService,logHandler:LogHandlerService) {
+    super(commonService,logHandler);
+    this.viewModel = {};
+
+  }
+
+  ngOnInit(){
+    this._commonService.dismissLoader();
+  }
+
+
+
 
 }
