070773d49f3d845ec5f14e20619728dca8602b77
Musaib <musaib709@outlook.com>
2025-09-30 17:20:12 +0530

    updated sever.ts and budget

--- src/server.ts
+++ src/server.ts
@@ -10,19 +10,7 @@ import { join } from 'node:path';
 const browserDistFolder = join(import.meta.dirname, '../browser');
 
 const app = express();
-const angularApp = new AngularNodeAppEngine();
-
-/**
- * Example Express Rest API endpoints can be defined here.
- * Uncomment and define endpoints as necessary.
- *
- * Example:
- * ```ts
- * app.get('/api/{*splat}', (req, res) => {
- *   // Handle API request
- * });
- * ```
- */
+const angularApp = new AngularNodeAppEngine(); // 👈 no arguments
 
 /**
  * Serve static files from /browser
@@ -49,7 +37,6 @@ app.use((req, res, next) => {
 
 /**
  * Start the server if this module is the main entry point.
- * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
 if (isMainModule(import.meta.url)) {
   const port = process.env['PORT'] || 4000;
@@ -57,12 +44,11 @@ if (isMainModule(import.meta.url)) {
     if (error) {
       throw error;
     }
-
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
 }
 
 /**
- * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
+ * Request handler used by Angular CLI (for dev-server and during build).
  */
 export const reqHandler = createNodeRequestHandler(app);
