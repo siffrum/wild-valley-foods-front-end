271f451c1a65626796e41a5ef7426ab9904e1542
musaib06 <musaib709@gmail.com>
2025-11-28 00:15:25 +0530

    updated configurations

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   apiResponseCacheTimeoutInMinutes: 5,
   enableResponseCacheProcessing: true,
   applicationVersion: '0.0.1',
-   apiBaseUrl: 'https://wild-valley-foods-backend-api.onrender.com',
+   apiBaseUrl: 'https://api.wildvalleyfoods.in',
   apiDefaultTimeout: 10,
   LoggingInfo: {
     cacheLogs: false,
